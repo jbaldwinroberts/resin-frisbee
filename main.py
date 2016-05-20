3ae6b7666fcf2dbfcdaaf6f018b31f6426f66f45
--replace-all <joe@animustechnologies.com>
2016-05-20 11:13:47 +0100

    init

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ while True:
     fusionPose = data["fusionPose"]
     print("r: %f p: %f y: %f" % (math.degrees(fusionPose[0]), math.degrees(fusionPose[1]), math.degrees(fusionPose[2])))
     gyro = data["gyro"]
-    print("r: %f p: %f y: %f" % (math.degrees(gyro[0]), math.degrees(gyro[1]), math.degrees(gyro[2])))
+    print("r: %f p: %f y: %f" % (gyro[0], gyro[1], gyro[2]))
 
     time.sleep(poll_interval*1.0/1000.0)
 
