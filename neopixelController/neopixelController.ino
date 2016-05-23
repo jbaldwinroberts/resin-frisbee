72561efc25aa4d75a51e1766ae7047b9dbfbf052
--replace-all <joe@animustechnologies.com>
2016-05-23 17:42:35 +0100

    changed order

--- neopixelController/neopixelController.ino
+++ neopixelController/neopixelController.ino
@@ -15,6 +15,56 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, 6, NEO_GRB + NEO_KHZ800);
 STATE state = Initialising;
 int frequency = 1;
 
+void flash(uint32_t c)
+{
+  enable(c);
+  delay(1000 / frequency);
+  disable();
+  delay(1000 / frequency);
+}
+
+void theaterChase(uint32_t c, uint8_t wait) {
+  for (int j=0; j<50; j++) {  //do 10 cycles of chasing
+    for (int q=0; q < 3; q++) {
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, c);    //turn every third pixel on
+      }
+      strip.show();
+
+      delay(wait);
+
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+      }
+    }
+  }
+}
+
+void enable(uint32_t c)
+{
+  for(uint16_t i=0; i < strip.numPixels(); i++) strip.setPixelColor(i, c);
+  strip.show();
+}
+
+void disable()
+{
+  for(uint16_t i=0; i < strip.numPixels(); i++) strip.setPixelColor(i, strip.Color(0, 0, 0));
+  strip.show();
+}
+
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
+
 void setup()
 {
   strip.begin();
@@ -71,54 +121,3 @@ void loop()
     }
   }
 }
-
-void flash(uint32_t c)
-{
-  enable(c);
-  delay(1000 / frequency);
-  disable();
-  delay(1000 / frequency);
-}
-
-void theaterChase(uint32_t c, uint8_t wait) {
-  for (int j=0; j<50; j++) {  //do 10 cycles of chasing
-    for (int q=0; q < 3; q++) {
-      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, c);    //turn every third pixel on
-      }
-      strip.show();
-
-      delay(wait);
-
-      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, 0);        //turn every third pixel off
-      }
-    }
-  }
-}
-
-void enable(uint32_t c)
-{
-  for(uint16_t i=0; i < strip.numPixels(); i++) strip.setPixelColor(i, c);
-  strip.show();
-}
-
-void disable()
-{
-  for(uint16_t i=0; i < strip.numPixels(); i++) strip.setPixelColor(i, strip.Color(0, 0, 0));
-  strip.show();
-}
-
-uint32_t Wheel(byte WheelPos) {
-  WheelPos = 255 - WheelPos;
-  if(WheelPos < 85) {
-    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-  }
-  if(WheelPos < 170) {
-    WheelPos -= 85;
-    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  }
-  WheelPos -= 170;
-  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-}
-
